2dc30ef810b60ec5f85e6e9805ce67d2d2929bd7
NASIF IQBAL <150609244+Nasif-Iqbal@users.noreply.github.com>
2025-08-07 11:01:56 +0600

    Create Kruskal's Algorithm.cpp

---
+++ Kruskal's Algorithm.cpp
@@ -0,0 +1,80 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N=1e5+10;
+int parent[N];
+int size[N];
+
+void make(int v)
+{
+    parent[v]=v;
+    size[v]=1;
+}
+int find(int v)
+{
+    if(v==parent[v])return v;
+    return parent[v]=find(parent[v]);
+}
+void Union(int a,int b)
+{
+    a=find(a);
+    b=find(b);
+    if(a!=b)
+    {
+        if(size[a]<size[b])
+            swap(a,b);
+        parent[b]=a;
+        size[a]+=size[b];
+    }
+}
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    vector<pair<int,pair<int,int>>>edges;
+    for(int i=0; i<m; i++)
+    {
+        int u,v,wt;
+        cin>>u>>v>>wt;
+        edges.push_back({wt,{u,v}});
+    }
+    sort(edges.begin(),edges.end());
+    for(int i=1; i<=n; i++)
+    {
+        make(i);
+    }
+    int total_cost=0;
+    for(int i=0; i<m; i++)
+    {
+        int wt=edges[i].first;
+        int u=edges[i].second.first;
+        int v=edges[i].second.second;
+        if(find(u)==find(v))continue;
+        Union(u,v);
+        total_cost+=wt;
+        cout<<u<<" "<<v<<endl;
+    }
+    cout<<total_cost<<endl;
+}
+
+/// time complexity : O((V+E)log(V))
+/*
+6 9
+5 4 9
+1 4 1
+5 1 4
+4 3 5
+4 2 3
+1 2 2
+3 2 3
+3 6 8
+2 6 7
+
+output:
+1 4
+1 2
+3 2
+5 1
+2 6
+17
+*/
